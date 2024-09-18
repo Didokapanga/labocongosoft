245587f6ecaa17d141b40a75552b4bc00ab471c4
Dido Kapanga <didokapanga@outlook.com>
2024-09-18 19:29:03 +0200

    services components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../components/Hero/Hero'
+import Services from '../../components/Services/Services'
 
 const Home = () => {
     return (
         <div className='home'>
             <Hero />
+            <Services />
         </div>
     )
 }
