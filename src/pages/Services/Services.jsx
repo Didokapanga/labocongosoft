0873038bf682dd6499d01486a9eef1cef190431c
Dido Kapanga <didokapanga@outlook.com>
2024-10-08 08:10:25 +0200

    page services 3

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Services.css'
-import ServicesHero from '../../components/Service/servicesHero'
 import ServiceCard from '../../components/Service/ServiceCard'
 import ServiceInfo from '../../components/Service/ServiceInfo'
 import Testimonials from '../../components/Testimonials/Testimonials'
@@ -8,11 +7,12 @@ import Agence from '../../components/Agence/Agence'
 import Faq from '../../components/Home/FAQ/Faq'
 import Footer from '../../components/Footer/Footer'
 import Copyright from '../../components/Copyright/Copyright'
+import ServicesHeader from '../../components/Service/ServicesHeader'
 
 const Services = () => {
     return (
         <div className='services'>
-            <ServicesHero />
+            <ServicesHeader />
             <ServiceCard />
             <ServiceInfo />
             <Testimonials />
